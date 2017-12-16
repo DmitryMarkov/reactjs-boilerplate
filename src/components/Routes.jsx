74ce9230dcb8bd4f507fcfab7fc734895235491c
Piotr Sikora <piotr.sikora82@gmail.com>
2017-12-16 03:43:41 +0100

    Fixed problem with react-router-dom during dist build  (changed to HashRouter)

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Route
+  HashRouter as Router,
+  Route,
+  browserHistory
 } from 'react-router-dom';
 import App from './App/App';
 import HelloWorld from './HelloWorld/HelloWorld';
 
 const Routes = () => (
-  <Router>
+  <Router history={browserHistory}>
     <div className="routes">
       <Route exact path="/" component={App} />
       <Route path="/hello-world" component={HelloWorld} />

ca592768f49676b96f88912c41ccf404cdf15fc1
crazyjuice <piotr.sikora82@gmail.com>
2018-07-06 11:40:54 +0200

    Added new rules for prettier, fixed eslint errors

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { hot } from 'react-hot-loader';
-import Routes from './components/Routes';
-import './assets/css/main.scss';
-import '../public/favicon.ico';
+import React from 'react'
+import { render } from 'react-dom'
+import { hot } from 'react-hot-loader'
+import Routes from './components/Routes'
+import './assets/css/main.scss'
+import '../public/favicon.ico'
 
-const App = () => <Routes />;
+const App = () => <Routes />
 
-render(App(), document.getElementById('app'));
+render(App(), document.getElementById('app'))
 
-export default hot(module)(App);
+export default hot(module)(App)
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Home.scss';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Home.scss'
 
 const Home = () => (
   <div className="home">
     <div className="home-info">{`${APP_NAME} v${APP_VERSION}`}</div>
     <Link to="/hello-world">Go to HelloWorld</Link>
   </div>
-);
+)
 
-export default Home;
+export default Home
